6ed71b438aa6f32aff75d16c55c5c4009651543b
dkemberg <damien.kemberg@pharmagest.com>
2018-12-13 11:31:50 +0100

    First tests

--- src/app/shared/directives/connected.directive.spec.ts
+++ src/app/shared/directives/connected.directive.spec.ts
@@ -2,7 +2,7 @@ import { ConnectedDirective } from './connected.directive';
 
 describe('ConnectedDirective', () => {
   it('should create an instance', () => {
-    const directive = new ConnectedDirective();
+    const directive = new ConnectedDirective(null);
     expect(directive).toBeTruthy();
   });
 });
--- src/app/shared/progress-bar/progress-bar.component.spec.ts
+++ src/app/shared/progress-bar/progress-bar.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProgressBarComponent } from './progress-bar.component';
 
-describe('ProgressBarComponent', () => {
+fdescribe('ProgressBarComponent', () => {
   let component: ProgressBarComponent;
   let fixture: ComponentFixture<ProgressBarComponent>;
 
@@ -22,4 +22,40 @@ describe('ProgressBarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should set start value at 0', () => {
+    expect(component.value).toEqual(0);
+  });
+
+  it('should toggle running flag when starting', () => {
+    expect(component.running).toBeFalsy();
+    component.start();
+    expect(component.running).toBeTruthy();
+  });
+
+  it('should toggle running flag when stopping', () => {
+    component.start();
+    expect(component.running).toBeTruthy();
+    component.end();
+    expect(component.running).toBeFalsy();
+  });
+
+  it('should stop counting when stopping', (done) => {
+    component.start();
+    
+    const subs = component.interval$.subscribe(
+      value => {},
+      error => {},
+      () => { 
+        //expect(subs).toBeFalsy();
+        done();
+      }
+    );
+
+    component.end();
+
+    
+
+  });
+
 });
