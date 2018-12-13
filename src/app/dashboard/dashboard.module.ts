f5330a2bf679715ebec2464efd819946f85453b4
dkemberg <damien.kemberg@pharmagest.com>
2018-12-13 14:52:55 +0100

    Add room management (add, edit, delete)

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RoomsComponent } from './rooms/rooms.component';
 import { DashboardRoutingModule } from './dashboard.routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [RoomsComponent],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class DashboardModule { }
--- src/app/dashboard/rooms/rooms.component.ts
+++ src/app/dashboard/rooms/rooms.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Room } from '../classes/room';
+import { environment } from 'src/environments/environment';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-rooms',
@@ -7,9 +12,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoomsComponent implements OnInit {
 
-  constructor() { }
+  rooms: Room[];
+  roomNameControl = new FormControl("", Validators.required);
+  currentRoom: Room;
+
+  constructor(private http: HttpClient, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.getRooms();
+  }
+
+  createRoom(): void {
+
+    if(this.currentRoom) {
+      this.http.put(environment.apiUrl + 'rooms/' + this.currentRoom.id, {name : this.roomNameControl.value})
+      .subscribe(
+        value => {
+          this.currentRoom = null;
+          this.roomNameControl.reset();
+          this.getRooms();}
+        )
+        
+    } else {
+      this.http.post(environment.apiUrl + 'rooms', {name : this.roomNameControl.value})
+      .subscribe(
+        value => {
+          console.log({value});
+          this.getRooms();}
+        )
+    }
+    
+  }
+
+  private getRooms() {
+    this.http.get<Room[]>(environment.apiUrl + 'rooms')
+      .subscribe(rooms => this.rooms = rooms);
+  }
+
+  edit(room: Room): void {
+    this.currentRoom = room;
+    this.roomNameControl.patchValue(room.name);
+  }
+
+  delete(room: Room) : void {
+    this.http.delete(environment.apiUrl + 'rooms/' + room.id ).subscribe(
+      value => {
+        this.getRooms();}
+      )
   }
 
 }
